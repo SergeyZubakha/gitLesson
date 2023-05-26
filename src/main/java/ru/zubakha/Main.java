591720c7a06d892f4a7316143e7bea18b6ea4d7c
Sergey Zubakha <qasergeyzubakha@yandex.ru>
2023-05-26 12:37:21 +0300

    new var

--- src/main/java/ru/zubakha/Main.java
+++ src/main/java/ru/zubakha/Main.java
@@ -2,7 +2,7 @@ package ru.zubakha;
 
 public class Main {
     public static void main(String[] args) {
-        String message = "Hello, qaguru!!!";
-        System.out.println(message);
+        String var1 = "Hello, qaguru!!!";
+        System.out.println(var1);
     }
 }
